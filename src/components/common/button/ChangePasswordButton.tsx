74afc7fc3bc70ea012775be8634784c013e50a88
Hohuuthien <hohuuthien270598@gmail.com>
2025-07-19 00:16:31 +0700

    update UI change password

--- src/components/common/button/ChangePasswordButton.tsx
+++ src/components/common/button/ChangePasswordButton.tsx
@@ -1,100 +1,183 @@
-import React from 'react'
+import React from "react";
 import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Button } from "@/components/ui/button"
-import { KeyRound } from "lucide-react";
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { KeyRound, Lock } from "lucide-react";
 import { useState } from "react";
-import { Eye, EyeOff } from "lucide-react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import CustomInput from "../input/CustomInput";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { toast } from "sonner";
+const formSchema = z
+  .object({
+    currentPassword: z.string().min(6, "Vui lòng nhập mật khẩu hiện tại"),
+    newPassword: z.string().min(6, "Vui lòng nhập mật khẩu mới"),
+    confirmNewPassword: z
+      .string()
+      .min(6, "Vui lòng nhập xác nhận lại mật khẩu mới"),
+  })
+  .refine((data) => data.newPassword === data.confirmNewPassword, {
+    path: ["confirmNewPassword"],
+    message: "Mật khẩu xác nhận không khớp",
+  });
 const ChangePasswordButton = () => {
-    const [showCurrentPassword, setCurrentShowPassword] = useState(false);
-    const [showNewPassword, setNewShowPassword] = useState(false);
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-    return (
-        <Dialog>
-            <form>
-                <DialogTrigger asChild>
-                    <Button
-                        variant="outline"
-                        className="flex items-center space-x-2 whitespace-nowrap cursor-pointer !rounded-button"
-                    >
-                        <KeyRound />
-                        <span>Change password</span>
-                    </Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px]">
-                    <DialogHeader>
-                        <DialogTitle>Change Password</DialogTitle>
-                        <DialogDescription>
+  const [isShowError, setIsShowError] = useState<boolean>(false);
 
-                        </DialogDescription>
-                    </DialogHeader>
-                    <div className="grid gap-4">
-                        <div className="grid gap-3">
-                            <Label htmlFor="name-1">Current Password</Label>
-                            <div className="relative">
-                                <Input type={showCurrentPassword ? "text" : "password"} id="name-1" name="currentPassword" placeholder="Type the current password" />
-                                <button
-                                    type="button"
-                                    onClick={() => setCurrentShowPassword((p) => !p)}
-                                    className="absolute right-3 top-1/2 -translate-y-1/2"
-                                >
-                                    {showCurrentPassword ? <Eye /> : <EyeOff />}
-                                </button>
-                            </div>
+  type ChangePasswordForm = z.infer<typeof formSchema>;
 
+  const form = useForm<ChangePasswordForm>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      currentPassword: "",
+      newPassword: "",
+      confirmNewPassword: "",
+    },
+  });
 
-                        </div>
-                        <div className="grid gap-3">
-                            <Label htmlFor="username-1">New Password</Label>
-                            <div className="relative">
-                                <Input type={showNewPassword ? "text" : "password"} name="newPassword" placeholder="Type new password" />
-                                <button
-                                    type="button"
-                                    onClick={() => setNewShowPassword((p) => !p)}
-                                    className="absolute right-3 top-1/2 -translate-y-1/2"
-                                >
-                                    {showNewPassword ? <Eye /> : <EyeOff />}
-                                </button>
-                            </div>
+  function onSubmit(data: ChangePasswordForm) {
+    toast("You submitted the following values", {
+      description: (
+        <pre className="mt-2 w-[320px] rounded-md bg-neutral-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+  const handleOnChange = () => {
+    if (isShowError) {
+      setIsShowError(false);
+    }
+  };
 
-                        </div>
-                        <div className="grid gap-3">
-                            <Label htmlFor="username-1">Confirm Password</Label>
-                            <div className="relative">
-                                <Input type={showConfirmPassword ? "text" : "password"} name="confirmPassword" placeholder="Type confirm password" />
-                                <button
-                                    type="button"
-                                    onClick={() => setShowConfirmPassword((p) => !p)}
-                                    className="absolute right-3 top-1/2 -translate-y-1/2"
-                                >
-                                    {showConfirmPassword ? <Eye /> : <EyeOff />}
-                                </button>
-                            </div>
-
-                        </div>
-                    </div>
-                    <DialogFooter>
-                        <DialogClose asChild>
-                            <Button variant="outline">Cancel</Button>
-                        </DialogClose>
-                        <Button variant="outline" type="submit">Save</Button>
-                    </DialogFooter>
-                </DialogContent>
-            </form>
-        </Dialog>
-
-
-    )
-}
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex items-center space-x-2 whitespace-nowrap cursor-pointer !rounded-button"
+        >
+          <KeyRound />
+          <span>Change password</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Change Password</DialogTitle>
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form className="w-full " onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="space-y-3">
+              <FormField
+                control={form.control}
+                name="currentPassword"
+                render={({ field }) => (
+                  <FormItem className="gap-1">
+                    <FormLabel className="text-txt-tertiary font-medium text-base">
+                      Current Password
+                    </FormLabel>
+                    <FormControl>
+                      <CustomInput
+                        prefixIcon={Lock}
+                        placeholder="Enter current password here"
+                        field={field}
+                        onChange={(e) => {
+                          field.onChange(e);
+                          handleOnChange();
+                        }}
+                        hasError={!!form.formState.errors.currentPassword}
+                        isPassword
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="newPassword"
+                render={({ field }) => (
+                  <FormItem className="gap-1">
+                    <FormLabel className="text-txt-tertiary font-medium text-base">
+                      New Password
+                    </FormLabel>
+                    <FormControl>
+                      <CustomInput
+                        prefixIcon={Lock}
+                        placeholder="Enter new password here"
+                        field={field}
+                        onChange={(e) => {
+                          field.onChange(e);
+                          handleOnChange();
+                        }}
+                        hasError={!!form.formState.errors.newPassword}
+                        isPassword
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmNewPassword"
+                render={({ field }) => (
+                  <FormItem className="gap-1">
+                    <FormLabel className="text-txt-tertiary font-medium text-base">
+                      Confirm New Password
+                    </FormLabel>
+                    <FormControl>
+                      <CustomInput
+                        prefixIcon={Lock}
+                        placeholder="Enter confirm new password here"
+                        field={field}
+                        onChange={(e) => {
+                          field.onChange(e);
+                          handleOnChange();
+                        }}
+                        hasError={!!form.formState.errors.confirmNewPassword}
+                        isPassword
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="mt-3">
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button
+                variant="outline"
+                type="submit"
+                className=" bg-blue-500 hover:bg-blue-600 text-white hover:text-white"
+              >
+                Save
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default ChangePasswordButton;
