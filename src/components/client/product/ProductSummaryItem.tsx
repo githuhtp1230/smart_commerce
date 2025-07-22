a5f6d7894dc2b3f7ad8f25f040246b488454f3b3
Hohuuthien <hohuuthien270598@gmail.com>
2025-07-22 22:03:09 +0700

    update style productpage and product detail

--- src/components/client/product/ProductSummaryItem.tsx
+++ src/components/client/product/ProductSummaryItem.tsx
@@ -2,11 +2,17 @@ import { RatingFilter } from "@/components/common/RatingFilter";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { PATH } from "@/constants/path";
-import { cn } from "@/lib/utils";
 import type { IProductSummary } from "@/type/products";
-import { ShoppingCart, X } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
 import React from "react";
 import { Link } from "react-router-dom";
+
+const formatPrice = (price: number) => {
+  return price.toLocaleString("vi-VN", {
+    style: "decimal",
+    minimumFractionDigits: 0,
+  });
+};
 
 interface Props {
   product: IProductSummary;
@@ -20,7 +26,7 @@ const ProductSummaryItem = ({ product }: Props) => {
   return (
     <Link to={`${PATH.PRODUCTS}/${product.id}`} className="group">
       <Card className="bg-primary shadow-xs p-0">
-        <img src={product.image} className="h-50 rounded-lg" />
+        <img src={product.image} className="h-60 rounded-lg object-contain  " />
         <div className="p-3">
           <h1 className="text-base group-hover:text-blue-400">
             {product.name}
@@ -33,16 +39,15 @@ const ProductSummaryItem = ({ product }: Props) => {
             <div className="flex items-center gap-2">
               {salePrice && (
                 <p className="line-through font-light text-txt-secondary">
-                  ${product.price}
+                  {formatPrice(product.price)} đ
                 </p>
               )}
-              <h1 className="font-semibold text-3xl">
-                ${salePrice ?? product.price}
+              <h1 className="font-semibold text-xl">
+                {formatPrice(salePrice ?? product.price)} đ
               </h1>
             </div>
             <Button className="bg-blue-400 hover:bg-blue-400">
               <ShoppingCart className="text-white" />
-              <span className="text-sm text-white">Add to cart</span>
             </Button>
           </div>
         </div>
