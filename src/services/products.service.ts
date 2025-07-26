33948f2fc2f3ce30706d04926ce6b0f041f425b2
Phuong <thephuongop12@gmail.com>
2025-07-26 13:16:20 +0700

    refactor call api product summaries

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -7,23 +7,17 @@ type PaginationProductSummaries = PaginationResponse<IProductSummary>;
 export const fetchProductSummaries = async (
   queryParams: URLSearchParams
 ): Promise<PaginationProductSummaries> => {
-  try {
-    const res = await httpRequest.get(`products?${queryParams.toString()}`);
-    const data = res.data.data as PaginationProductSummaries;
-    return data;
-  } catch (error) {
-    throw error;
-  }
+  const res = await httpRequest.get(
+    `products/summaries?${queryParams.toString()}`
+  );
+  const data = res.data.data as PaginationProductSummaries;
+  return data;
 };
 
 export const fetchProductDetail = async (
   productId: number
 ): Promise<IProductDetail> => {
-  try {
-    const res = await httpRequest.get(`products/${productId}`);
-    const data = res.data.data as IProductDetail;
-    return data;
-  } catch (error) {
-    throw error;
-  }
+  const res = await httpRequest.get(`products/summaries/${productId}`);
+  const data = res.data.data as IProductDetail;
+  return data;
 };
