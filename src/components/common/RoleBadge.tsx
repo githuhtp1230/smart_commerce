412287cf34062385b6702cb7e3cc7cdf78d11015
nguyenphu2323 <phunguyen23282003@gmail.com>
2025-07-27 16:45:12 +0700

    refactor custom role badge

--- src/components/common/RoleBadge.tsx
+++ src/components/common/RoleBadge.tsx
@@ -11,22 +11,22 @@ export const RoleBadge = ({ badgeColor, content }: Props) => {
         switch (badgeColor) {
             case "admin":
                 return (
-                    <Badge className="bg-foreground-system-success text-txt-system-success">
-                        <div><ShieldCheck /></div>
+                    <Badge className="bg-foreground-system-success text-txt-system-success w-[100px]">
+                        <div><ShieldCheck /> {" "}</div>
                         {content}
                     </Badge>
                 );
             case "manage":
                 return (
-                    <Badge className="bg-foreground-system-information text-txt-system-information">
-                        <div><UserRoundCog /></div>
+                    <Badge className="bg-foreground-system-information text-txt-system-information w-[100px]">
+                        <div><UserRoundCog /> {" "}</div>
                         {content}
                     </Badge>
                 );
             case "user":
                 return (
-                    <Badge className="bg-gray-200 text-gray-700">
-                        <div><Users /></div>
+                    <Badge className="bg-gray-200 text-gray-700 w-[100px]">
+                        <div><Users /> {" "}</div>
                         {content}
                     </Badge>
                 );
